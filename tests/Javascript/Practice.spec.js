012fd94fe5ab88e72561e1b53b2933c29c801c7d
deepanaucbe <deepanaucbe@gmail.com>
2026-01-30 12:57:53 +0530

    updated

--- tests/Javascript/Practice.spec.js
+++ tests/Javascript/Practice.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-test.setTimeout(160000)
+
 test('test', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
@@ -7,48 +7,29 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Claim' }).click();
-  await page.getByRole('button', { name: ' Assign Claim' }).click();
+  await page.getByRole('link', { name: 'Time' }).click();
+  await page.getByRole('listitem').filter({ hasText: 'Timesheets' }).locator('i').click();
+  await page.getByRole('menuitem', { name: 'Employee Timesheets' }).click();
   await page.getByRole('textbox', { name: 'Type for hints...' }).click();
-  await page.waitForLoadState('networkidle');
-  await page.getByRole('textbox', { name: 'Type for hints...' }).fill('1212'); 
-
-const employeeInput = page.locator(
-  'label:has-text("Employee Name") >> xpath=following::input[1]'
-);
-
-// Wait for autosuggest processing to finish
-await page.waitForTimeout(2000);
-
-// Read the actual input value
-const valueAfterSearch = await employeeInput.inputValue();
-
-// 🚨 If value is still the ID → No employee exists
-if (valueAfterSearch.trim() === '1212') {
-  console.log('❌ No user found');
-
-  await page.locator('header i.oxd-userdropdown-icon').click();
-  await page.getByRole('menuitem', { name: 'Logout' }).click();
-  return;
-}
-  
-  await page.getByText('Deepan deepu').first().click({ timeout: 150000 });
-  await page.getByText('-- Select --').first().click();
-  await page.getByText('Accommodation').click();
-  await page.getByText('-- Select --').click();
-  await page.getByText('Indian Rupee').click();
-  await page.locator('textarea').click();
-  await page.locator('textarea').fill('Stay claim');
-  await page.getByRole('button', { name: 'Create' }).click();
-  await page.getByRole('button', { name: ' Add' }).first().click();
-  await page.getByText('-- Select --').click();
-  await page.getByRole('option', { name: 'Accommodation' }).click();
-  await page.locator('.oxd-icon.bi-calendar').click();
-  await page.getByText('20', { exact: true }).click();
-  await page.locator('.oxd-grid-2 > div:nth-child(2) > .oxd-input-group > div:nth-child(2) > .oxd-input').click();
-  await page.locator('.oxd-input.oxd-input--focus').fill('5000');
+  await page.getByRole('textbox', { name: 'Type for hints...' }).fill('Orange Test');
+  await page.getByRole('option', { name: 'Orange Test' }).click();
+  await page.locator('form').getByRole('button', { name: 'View' }).click();
+  await page.getByRole('button', { name: 'Edit' }).click();
+  await page.getByRole('textbox', { name: 'Type for hints...' }).click();
+  await page.getByRole('textbox', { name: 'Type for hints...' }).fill('Apache Software Foundation');
+  await page.getByText('Apache Software Foundation -').click();
+  await page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow').click();
+  await page.getByRole('option', { name: 'Bug Fixes' }).click();
+  await page.getByRole('textbox').nth(2).click();
+  await page.getByRole('textbox').nth(2).fill('8');
+  await page.getByRole('textbox').nth(3).click();
+  await page.getByRole('textbox').nth(3).fill('8');
+  await page.getByRole('textbox').nth(4).click();
+  await page.getByRole('textbox').nth(4).fill('8');
+  await page.getByRole('textbox').nth(5).click();
+  await page.getByRole('textbox').nth(5).fill('8');
+  await page.locator('td:nth-child(7) > .oxd-input-group > div:nth-child(2) > .oxd-input').click();
+  await page.locator('.oxd-input.oxd-input--focus').fill('8');
   await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('button', { name: 'Submit' }).click();
-  await page.locator('header i.oxd-userdropdown-icon').click({ timeout: 30000 });
-  await page.getByRole('menuitem', { name: 'Logout' }).click();
 });
