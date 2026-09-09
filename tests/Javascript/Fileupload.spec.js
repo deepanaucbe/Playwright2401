9c44e0428f622e5d79801fb5450d63e81b37c62e
deepanaucbe <deepanaucbe@gmail.com>
2026-09-09 20:59:19 +0530

    update

--- tests/Javascript/Fileupload.spec.js
+++ tests/Javascript/Fileupload.spec.js
@@ -2,10 +2,10 @@ import { test, expect } from'@playwright/test'
 
 test('Fileupload', async ({ page }) => {
 await page.goto("https://testautomationpractice.blogspot.com/")
-const Singlefile=await page.locator('[id="singleFileInput"]')
-await Singlefile.setInputFiles('tests/Files/Data.pdf');
+const Singlefile=page.locator('[id="singleFileInput"]')
+await Singlefile.setInputFiles('tests/Files/123.pdf');
 
-const Multiplefile=await page.locator('[id="multipleFilesInput"]')
-await Multiplefile.setInputFiles(['tests/Files/Data.pdf','tests/Files/Data.jpeg']);
+//const Multiplefile=page.locator('[id="multipleFilesInput"]')
+//await Multiplefile.setInputFiles(['tests/Files/Data.pdf','tests/Files/Data.jpeg']);
 
 })
--- tests/Javascript/Test.spec.js
+++ tests/Javascript/Test.spec.js
@@ -1,8 +1,14 @@
-const response = await request.post('https://reqres.in/api/users', {
-  data: {
-    name: "Deepan",
-    job: "QA"
-  }
-});
+import {test,expect} from '@playwright/test'
+import path from 'path';
+test('upload file and verify file name',async ({page}) =>{
+await page.goto("https://the-internet.herokuapp.com/upload")
+const filePath= 'c:\\Sample.txt'
+const expectedFileName= path.basename(filePath)
+await page.locator('#file-upload').setInputFiles(filePath)
+await page.locator('#file-submit').click()
+const uploadedFileName= await page.locator('#uploaded-files').textContent()
+console.log(uploadedFileName)
+expect (uploadedFileName.trim()).toBe(expectedFileName)
 
-expect(response.status()).toBe(201);
+}
+)
